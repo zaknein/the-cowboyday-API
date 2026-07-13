3b3d29a596f36f37bce03328d703d3853902094a
German Kröger <gkroger11@gmail.com>
2026-07-13 21:10:26 +0200

    securityconfig

---
+++ src/main/java/com/zaknein/the_cowboyday_API/SecurityConfig.java
@@ -0,0 +1,83 @@
+package com.zaknein.the_cowboyday_API;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+
+import com.zaknein.TicTacToeAPI.security.JwtConverter;
+
+import lombok.AllArgsConstructor;
+
+
+
+
+
+
+
+
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+
+public class SecurityConfig {
+
+
+    private final RsaKeyProperties rsaKeys;
+    private final JwtConverter jwtConverter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
+
+        return http.csrf(csrf -> csrf.disable())
+                    .authorizeHttpRequests(req ->
+                        req
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/holapublic/**").permitAll()
+                        .anyRequest().authenticated()
+                    )
+                    .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtConverter)))
+                    .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                    .build();
+
+
+
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(rsaKeys.publicKey()).build();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(rsaKeys.publicKey())
+                .privateKey(rsaKeys.privateKey())
+                .build();
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
