923d072031d7d9ca820ce8595167b0d933ab1b47
German Kröger <gkroger11@gmail.com>
2026-07-13 16:44:38 +0200

    initial commit

---
+++ src/main/java/com/zaknein/the_cowboyday_API/TheCowboydayApiApplication.java
@@ -0,0 +1,13 @@
+package com.zaknein.the_cowboyday_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheCowboydayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheCowboydayApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zaknein/the_cowboyday_API/TheCowboydayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaknein.the_cowboyday_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheCowboydayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
