75e53d39165ff99dc04594fba119ae15101ff7ef
German Kröger <gkroger11@gmail.com>
2026-07-13 21:08:19 +0200

    rsakeyproperties

---
+++ src/main/java/com/zaknein/the_cowboyday_API/RsaKeyProperties.java
@@ -0,0 +1,8 @@
+package com.zaknein.the_cowboyday_API;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "rsa")
+public record RsaKeyProperties(RSAPublicKey publicKey, RSAPrivateKey privateKey) {}
