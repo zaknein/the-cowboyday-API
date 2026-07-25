e1280a1d7b6590f3d2faeef88150ad87273e3eb3
German Kröger <gkroger11@gmail.com>
2026-07-25 13:29:50 +0200

    get titulos

---
+++ src/main/java/com/zaknein/the_cowboyday_API/controller/TitulosController.java
@@ -0,0 +1,32 @@
+package com.zaknein.the_cowboyday_API.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@AllArgsConstructor
+@RequestMapping("/titulos/")
+@RestController
+public class TitulosController {
+
+
+    @GetMapping()
+    public List<titulos> getTitulos() {
+        return titulosService.getTitulos();
+    }
+    
+
+    
+    
+}
