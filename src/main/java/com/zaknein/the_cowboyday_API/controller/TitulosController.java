66e07ad92b34ac815f15d2bbd62b828e22dec3cf
German Kröger <gkroger11@gmail.com>
2026-07-25 13:31:14 +0200

    post titulos

--- src/main/java/com/zaknein/the_cowboyday_API/controller/TitulosController.java
+++ src/main/java/com/zaknein/the_cowboyday_API/controller/TitulosController.java
@@ -12,6 +12,9 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
@@ -26,7 +29,12 @@ public class TitulosController {
         return titulosService.getTitulos();
     }
     
-
+    @PostMapping()
+    public String createTitulos(@RequestBody String entity) {
+        
+        return titulosService.createTitulos();
+    }
     
+
     
 }
