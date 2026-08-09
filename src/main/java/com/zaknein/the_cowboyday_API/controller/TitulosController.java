33dea3e3079728c0f531279088f34bc1e66042c9
German Kröger <gkroger11@gmail.com>
2026-08-09 16:35:39 +0200

    controller delete titulos

--- src/main/java/com/zaknein/the_cowboyday_API/controller/TitulosController.java
+++ src/main/java/com/zaknein/the_cowboyday_API/controller/TitulosController.java
@@ -11,6 +11,7 @@ import lombok.AllArgsConstructor;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -38,6 +39,10 @@ public class TitulosController {
         return titulosService.createTitulos(titulos);
     }
     
+    @DeleteMapping("/{id}")
+    public void deleteTitulo(@PathVariable Long id){
+        return titulosService.deleteTitulos(id);
+    }
 
     
 }
